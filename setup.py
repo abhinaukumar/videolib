7b017dfd65e843a149cd8fb10cffc9b0651a38a1
Abhinau Kumar <ab.kumr98@gmail.com>
2022-10-08 00:32:24 -0500

    Added setup and requirements files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='videolib',
+    author='Abhinau Kumar',
+    author_email='ab.kumr98@gmail.com',
+    version='0.1.0',
+    url='https://github.com/abhinaukumar/videolib',
+    description='Package for easy Video IO and color conversion in Python.',
+    install_requires=['numpy', 'scikit-video', 'matplotlib'],
+    python_requires='>=3.7.0',
+    license='MIT License',
+    packages=find_packages()
+)
