0da6c82cd79b8f141b0e24852e9da72ed81073a2
Abhinau Kumar <ab.kumr98@gmail.com>
2024-04-02 15:39:24 -0500

    Updated dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ setup(
     version='0.2.0',
     url='https://github.com/abhinaukumar/videolib',
     description='Package for easy Video IO and color conversion in Python.',
-    install_requires=['numpy', 'scikit-video', 'matplotlib'],
+    install_requires=[
+        'numpy',
+        'scikit-video',
+        'matplotlib',
+        'imageio'
+    ],
     python_requires='>=3.7.0',
     license='MIT License',
     packages=find_packages()
